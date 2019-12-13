987bc8405c901272d769655ee45333ea4b89cd21
Sean R Reid <sean@torchcodelab.com>
2019-12-13 10:22:50 -0500

    Testing on my own machine

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,9 +1,10 @@
-import { Component, ElementRef, HostListener, Input, OnInit } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, ElementRef, HostListener, ViewChild } from "@angular/core";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-file-upload',
-  templateUrl: './file-upload.component.html',
+  selector: "app-file-upload",
+  templateUrl: "./file-upload.component.html",
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
@@ -11,33 +12,34 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
       multi: true
     }
   ],
-  styleUrls: ['./file-upload.component.scss']
+  styleUrls: ["./file-upload.component.scss"]
 })
 export class FileUploadComponent implements ControlValueAccessor {
-  @Input() progress;
   onChange: Function;
   private file: File | null = null;
 
-  @HostListener('change', ['$event.target.files']) emitFiles( event: FileList ) {
+  imageUpload = new FormGroup({
+    email: new FormControl(null, Validators.required),
+    image: new FormControl(null, [Validators.required, requiredFileType("png")])
+  });
+
+  @HostListener("change", ["$event.target.files"]) emitFiles(event: FileList) {
     const file = event && event.item(0);
     this.onChange(file);
     this.file = file;
   }
 
-  constructor( private host: ElementRef<HTMLInputElement> ) {
-  }
+  constructor(private host: ElementRef<HTMLInputElement>) {}
 
-  writeValue( value: null ) {
+  writeValue(value: null) {
     // clear file input
-    this.host.nativeElement.value = '';
+    this.host.nativeElement.value = "";
     this.file = null;
   }
 
-  registerOnChange( fn: Function ) {
+  registerOnChange(fn: Function) {
     this.onChange = fn;
   }
 
-  registerOnTouched( fn: Function ) {
-  }
-
+  registerOnTouched(fn: Function) {}
 }
